08cf2e7059ec717347d974935b7b285ce7a81591
Aditya Prakash <adityaprakash0904@gmail.com>
2025-09-09 23:24:06 +0530

    error-fix

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  PORT: process.env.PORT || 5000,
+  PORT:process.env.PORT || 5000,
   MONGODB_URI:
     process.env.MONGODB_URI ||
     "mongodb+srv://adityaprakash0904_db_user:uCavGKnC25jZxsDR@cluster0.liicml9.mongodb.net/apiPlayground",
--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const asyncHandler = require("../utils/asyncHandler");
-const Profile = require("../models/Profile");
+const Profile = require("../models/profile");
 
 // Get single profile (by email or first doc)
 router.get(
